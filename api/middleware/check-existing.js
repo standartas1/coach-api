06497500b983c9b43e51c5611691d039f76112fa
Laurynas Blockis <laurynas.blockis@gmail.com>
2019-12-17 20:52:07 +0200

    plssssss

--- api/middleware/check-existing.js
+++ api/middleware/check-existing.js
@@ -1,7 +1,7 @@
 const Skill = require('../models/skill');
 
 exports.existingskill = async (req, res, next) => {
-    try {
+    try { //
         console.log(req.body.skill);
         if (req.body.skill !== undefined) {
             const skill = await skill.findById(req.body.skill);
