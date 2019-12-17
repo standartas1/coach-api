074d44f33f25bd5e6a9278296c4703923d9f1342
Laurynas Blockis <laurynas.blockis@gmail.com>
2019-12-17 20:08:55 +0200

    final

--- api/middleware/user-check-auth.js
+++ api/middleware/user-check-auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.auth = (req, res, next) => {
     try {
+        //
         const token = req.headers.authorization.split(' ')[1];
         req.userData = jwt.verify(token, process.env.JWT_KEY);
         const userId = req.params.userId;
